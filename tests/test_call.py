b2c5a40ed7a19408eafc7e8731685aa82271b4d1
mangG907 <mang.gee@gmail.com>
2024-07-26 16:39:38 +0900

    what

---
+++ tests/test_call.py
@@ -0,0 +1,22 @@
+from mov.api.call import gen_url, req, get_key, req2dataframe
+
+def test_req2dataframe():
+    l = req2dataframe()
+    assert len(l) > 0
+    v = l[0]
+    assert 'rnum' in v.keys()
+    assert v['rnum'] == '1'
+
+def test_비밀키숨기기():
+    key = get_key()
+    assert key
+
+def test_gen_url():
+    url = gen_url()
+    assert "http" in url 
+    assert "kobis" in url
+
+def test_req():
+    code, data = req()
+    assert code == 200
+
