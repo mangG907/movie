795ca851f1a31f75e5a2b39a329c22829df979f6
mangG907 <mang.gee@gmail.com>
2024-07-26 11:25:08 +0900

    함수 추가

---
+++ api/call.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+def req2dataframe():
+    _, data = req()
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
+
+def get_key():
+    key = os.getenv('MOVIE_API_KEY')
+    return key
+
+def req():
+    url = gen_url()
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+
+    print(data)
+    return code, data
+
+def gen_url(dt="20120101"):
+    base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    key = get_key()
+    url = f"{base_url}?key={key}&targetDt={dt}"
+    
+    return url
