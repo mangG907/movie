2699b109ea6ebc9fadf923e3879af844acb21c58
mangG907 <mang.gee@gmail.com>
2024-07-29 10:53:41 +0900

    test ok

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,28 +1,27 @@
 import requests
 import os
-import pandas
+import pandas as pd
 
 def list2df():
-    l = req2dataframe()
+    l = req2list()
     df = pd.DataFrame(l)
     return df
 
-def req2dataframe():
+def req2list() -> list:
     _, data = req()
     l = data['boxOfficeResult']['dailyBoxOfficeList']
-    pf = list2df(l)
-    return pf
+    return l
 
 def get_key():
+    """영화진흫위원회 가입 및 AIP 키 생성 후 환경변수 선언 필요"""
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req():
-    url = gen_url()
+def req(dt='20200101'):
+    url = gen_url(dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
-
     print(data)
     return code, data
 
@@ -30,5 +29,4 @@ def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
-    
     return url
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,17 +1,35 @@
-# 테스트 코드는 검증하고 싶은 경로의 .py의 함수들을 import 한다. 
+from mov.api.call import gen_url, req, get_key, req2list, list2df
+import pandas as pd
 
-from mov.api.call import gen_url, req, get_key
+def test_list2df():
+    df = list2df()
+    print(df)
+    assert isinstance(df, pd.DataFrame)
+    assert 'rnum' in df.columns
+    assert 'openDt' in df.columns
+    assert 'movieNm' in df.columns
+    assert 'audiAcc' in df.columns
 
+def test_req2list():
+    l = req2list()
+    assert len(l) > 0
+    v = l[0]
+    assert 'rnum' in v.keys()
+    assert v['rnum'] == '1'
 
 def test_비밀키숨기기():
     key = get_key()
     assert key
 
-def test_gen_url():
+
+def test_유알엘테스트():
     url = gen_url()
-    assert "http" in url 
+    assert "http" in url
     assert "kobis" in url
 
 def test_req():
     code, data = req()
+    assert code == 200
+
+    code, data = req('20230101')
     assert code == 200
