945ff4a7137b3e191b69e155629a3760329d791c
mangG907 <mang.gee@gmail.com>
2024-07-31 14:53:41 +0900

    fix key

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -2,38 +2,78 @@ import requests
 import os
 import pandas as pd
 
-def save2df():
-    df = list2df()
-    df['load_dt'] = '20120101'
+
+def echo(yaho):
+    return yaho
+
+def apply_type2df(load_dt="20120101", path="~/tmp/test_parquet"):
+    df = pd.read_parquet(f'{path}/load_dt={load_dt}')
+    df['rnum'] = pd.to_numeric(df['rnum'])
+    df['rank'] = pd.to_numeric(df['rank'])
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',  
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 
+                'salesChange', 'audiInten', 'audiChange']
+    for c in num_cols:
+        df[c] = pd.to_numeric(df[c])
+        
+    #df[num_cols] = df[num_cols].apply(pd.to_numeric)    
+    return df
+
+def save_data(ds_nodash):
+    from mov.api.call import apply_type2df
+
+    df = apply_type2df(load_dt=ds_nodash)
+
+    print("*" * 33)
+    print(df.head(10))
+    print("*" * 33)
+
+    print(df.dtypes)
+
+    # 개봉일 기준 그룹핑 누적 관객수 합
+    g = df.groupby('openDt')
+    sum_df = g.agg({'audiCnt' : 'sum'}).reset_index()
+    print(sum_df)
+
+
+
+def save2df(load_dt='20120101'):
+    """airflow 호출 지점"""
+    df = list2df(load_dt)
+    df['load_dt'] = load_dt
     print(df.head(5))
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
 
-def list2df():
-    l = req2list()
+def list2df(load_dt='20120101'):
+    l = req2list(load_dt)
     df = pd.DataFrame(l)
     return df
 
-def req2list() -> list:
+def req2list(load_dt='20120101') -> list:
     _, data = req()
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
 def get_key():
-    """영화진흫위원회 가입 및 AIP 키 생성 후 환경변수 선언 필요"""
+    """영화진흥위원회 가입 및 AIP 키 생성 후 환경변수 선언 필요"""
     key = os.getenv('MOVIE_API_KEY')
+    print(key)
     return key
 
-def req(dt='20200101'):
-    url = gen_url(dt)
+def req(load_dt='20120101'):
+    url = gen_url(load_dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
     print(data)
     return code, data
 
-def gen_url(dt="20120101"):
+def gen_url(load_dt='20120101'):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
-    url = f"{base_url}?key={key}&targetDt={dt}"
+    url = f"{base_url}?key={key}&targetDt={load_dt}"
+    print("*" * 10)
+    print(url)
+    print("*" * 10)
     return url
