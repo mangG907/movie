24e2c33ade915c164be2fd92a4d8ff67ff16f0b6
mangG907 <mang.gee@gmail.com>
2024-07-31 19:08:21 +0900

    fix

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -24,11 +24,11 @@ def save_data(ds_nodash):
 
     df = apply_type2df(load_dt=ds_nodash)
 
-    print("*" * 33)
-    print(df.head(10))
-    print("*" * 33)
+    #print("*" * 33)
+    #print(df.head(10))
+    #print("*" * 33)
 
-    print(df.dtypes)
+    #print(df.dtypes)
 
     # 개봉일 기준 그룹핑 누적 관객수 합
     g = df.groupby('openDt')
@@ -39,41 +39,45 @@ def save_data(ds_nodash):
 
 def save2df(load_dt='20120101', url_param={}):
     """airflow 호출 지점"""
-    df = list2df(load_dt)
+    df = list2df(load_dt, url_param)
     df['load_dt'] = load_dt
-    print(df.head(5))
+    #print(df.head(5))
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
 
 def list2df(load_dt='20120101', url_param={}):
-    l = req2list(load_dt)
+    l = req2list(load_dt, url_param)
     df = pd.DataFrame(l)
     return df
 
-def req2list(load_dt='20120101') -> list:
-    _, data = req()
+def req2list(load_dt='20120101', url_param={}) -> list:
+    _, data = req(load_dt, url_param)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
-    return l
+    df = pd.DataFrame(l)
+    return df 
 
 def get_key():
     """영화진흥위원회 가입 및 AIP 키 생성 후 환경변수 선언 필요"""
     key = os.getenv('MOVIE_API_KEY')
-    print(key)
+    #print(key)
     return key
 
-def req(load_dt='20120101'):
-    url = gen_url(load_dt)
+def req(load_dt='20120101', url_param={}):
+    url = gen_url(load_dt, url_param)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
     print(data)
     return code, data
 
-def gen_url(load_dt='20120101'):
+def gen_url(load_dt='20120101', url_param={}):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={load_dt}"
-    print("*" * 10)
-    print(url)
-    print("*" * 10)
+    for k, v in url_param.items():
+        url = url + f"&{k}={v}"
+
+    #print("*" * 10)
+    #print(url)
+    #print("*" * 10)
     return url
