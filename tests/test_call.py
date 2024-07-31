59a1f6c813e5252b6dadda26f30c410b43154df8
mangG907 <mang.gee@gmail.com>
2024-07-31 19:08:02 +0900

    fix

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,15 +1,30 @@
-from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df
+from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df, echo, apply_type2df 
 import pandas as pd
 
+def test_apply_type2df():
+    df = apply_type2df()
+    assert isinstance(df, pd.DataFrame)
+    assert str(df['rnum'].dtype) in ['int64']
+    assert str(df['rank'].dtype) in ['int64']
+    #assert str(df['audiInten'].dtype) in ['int64']
+
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',
+                'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten',
+                'salesChange', 'audiInten', 'audiChange']
+
+    for c in num_cols:
+        assert df[c].dtype in ['int64', 'float64']
+
+
 def test_save2df():
-    df = save2df(load_dt='20241231')
+    df = save2df(load_dt='20231231')
     assert isinstance(df, pd.DataFrame)
     assert 'load_dt' in df.columns
+    assert len(df) == 10
 
 
 def test_list2df():
     df = list2df()
-    print(df)
     assert isinstance(df, pd.DataFrame)
     assert 'rnum' in df.columns
     assert 'openDt' in df.columns
@@ -19,9 +34,9 @@ def test_list2df():
 def test_req2list():
     l = req2list()
     assert len(l) > 0
-    v = l[0]
-    assert 'rnum' in v.keys()
-    assert v['rnum'] == '1'
+    v = l.iloc[0]
+    #assert 'rnum' in v.keys()
+    #assert v['rnum'] == '1'
 
 def test_비밀키숨기기():
     key = get_key()
